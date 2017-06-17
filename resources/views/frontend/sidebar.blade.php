dcade90540f1645e287ccee44d93f56cf8c59331
fazzyA <faz.pak@gmail.com>
2017-06-17 13:02:53 +0500

    package list in front view

--- resources/views/frontend/sidebar.blade.php
+++ resources/views/frontend/sidebar.blade.php
@@ -4,5 +4,6 @@
             <a href="{{ url('file/meta/create') }}" class="list-group-item">Meta data</a>
             <a href="{{ url('file/list')   }}" class="list-group-item">Files</a>
             <a href="{{ url('file/format') }}" class="list-group-item">File Formate</a>
+            <a href="{{url('/packages')}}" class="list-group-item">Package List</a>
         </div>
     </div>
