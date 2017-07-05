b2f57f2543a1e418249b85699d4db4cbab627a28
Sadaf Siddiqui <sadaf_cu@hotmail.com>
2017-07-05 20:56:50 +0500

    set layout #21

--- resources/views/analytics/index.blade.php
+++ resources/views/analytics/index.blade.php
@@ -1,1 +1,17 @@
-analytics page
+@extends('layouts.backend')
+
+@section('content')
+your text here
+@endsection
+
+@section('heading')
+    Analysis <small></small>
+@endsection
+
+@section('title')
+    Analytics
+@endsection
+
+@section('script')
+    @parent
+@endsection
