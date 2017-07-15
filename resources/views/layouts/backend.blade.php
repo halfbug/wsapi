ca33031360ad0a9af6341fcf7381cf60ef1e9b2a
Sadaf Siddiqui <sadaf_cu@hotmail.com>
2017-07-15 14:53:03 +0500

    link add

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -292,9 +292,9 @@
                             <li>
                                 <a href="{{url("file/list")}}"><i class="fa fa-files-o fa-fw"></i> Files</a>
                             </li>
-                            <li>
-                                <a href="{{ url('file/meta/create') }}"><i class="fa fa-plus-square fa-fw"></i> Add Meta data</a>
-                            </li>
+                            <!-- <li>
+                                <a href=""><i class="fa fa-plus-square fa-fw"></i> Add Meta data</a>
+                            </li> -->
                             <li>
                                 <a href="{{url("analysis/")}}"><i class="fa fa-diamond fa-fw"></i> Analysis</a>
                             </li>
@@ -332,90 +332,90 @@
                             <!-- /.nav-second-level -->
                         </li>
                             
-<!--                            <li>
-                                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                          <li>
+                                <a href="#"><i class="fa fa-cog fa-fw"></i> Settings<span class="fa arrow"></span></a>
                                 <ul class="nav nav-second-level">
                                     <li>
-                                        <a href="flot.html">Flot Charts</a>
+                                        <a href="{{ url('file/meta/create') }}"><i class="fa fa-plus-square fa-fw"></i>Set Meta Data</a>
                                     </li>
                                     <li>
-                                        <a href="morris.html">Morris.js")}} Charts</a>
+                                        <a href="{{ url('settings/index') }}"><i class="fa fa-feed fa-fw"></i>General Settings</a>
                                     </li>
                                 </ul>
-                                 /.nav-second-level 
+                                 {{--/.nav-second-level --}}
                             </li>
-                            <li>
-                                <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
-                            </li>
-                            <li>
-                                <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="panels-wells.html">Panels and Wells</a>
-                                    </li>
-                                    <li>
-                                        <a href="buttons.html">Buttons</a>
-                                    </li>
-                                    <li>
-                                        <a href="notifications.html">Notifications</a>
-                                    </li>
-                                    <li>
-                                        <a href="typography.html">Typography</a>
-                                    </li>
-                                    <li>
-                                        <a href="icons.html"> Icons</a>
-                                    </li>
-                                    <li>
-                                        <a href="grid.html">Grid</a>
-                                    </li>
-                                </ul>
-                                 /.nav-second-level 
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="#">Second Level Item</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Second Level Item</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level <span class="fa arrow"></span></a>
-                                        <ul class="nav nav-third-level">
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                        </ul>
-                                         /.nav-third-level 
-                                    </li>
-                                </ul>
-                                 /.nav-second-level 
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="blank.html">Blank Page</a>
-                                    </li>
-                                    <li>
-                                        <a href="login.html">Login Page</a>
-                                    </li>
-                                </ul>
-                                 /.nav-second-level 
-                            </li>-->
+                            <!--                              <li>
+                                                            <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
+                                                            <ul class="nav nav-second-level">
+                                                                <li>
+                                                                    <a href="panels-wells.html">Panels and Wells</a>
+                                                                </li>
+                                                                <li>
+                                                                    <a href="buttons.html">Buttons</a>
+                                                                </li>
+                                                                <li>
+                                                                    <a href="notifications.html">Notifications</a>
+                                                                </li>
+                                                                <li>
+                                                                    <a href="typography.html">Typography</a>
+                                                                </li>
+                                                                <li>
+                                                                    <a href="icons.html"> Icons</a>
+                                                                </li>
+                                                                <li>
+                                                                    <a href="grid.html">Grid</a>
+                                                                </li>
+                                                            </ul>
+                                                             /.nav-second-level
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+                                                            <ul class="nav nav-second-level">
+                                                                <li>
+                                                                    <a href="#">Second Level Item</a>
+                                                                </li>
+                                                                <li>
+                                                                    <a href="#">Second Level Item</a>
+                                                                </li>
+                                                                <li>
+                                                                    <a href="#">Third Level <span class="fa arrow"></span></a>
+                                                                    <ul class="nav nav-third-level">
+                                                                        <li>
+                                                                            <a href="#">Third Level Item</a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a href="#">Third Level Item</a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a href="#">Third Level Item</a>
+                                                                        </li>
+                                                                        <li>
+                                                                            <a href="#">Third Level Item</a>
+                                                                        </li>
+                                                                    </ul>
+                                                                     /.nav-third-level
+                                                                </li>
+                                                            </ul>
+                                                             /.nav-second-level
+                                                        </li>
+                                                        <li>
+                                                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+                                                            <ul class="nav nav-second-level">
+                                                                <li>
+                                                                    <a href="blank.html">Blank Page</a>
+                                                                </li>
+                                                                <li>
+                                                                    <a href="login.html">Login Page</a>
+                                                                </li>
+                                                            </ul>
+                                                             /.nav-second-level
+                                                        </li>-->
                         </ul>
                     </div>
                     <!-- /.sidebar-collapse -->
