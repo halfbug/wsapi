31e18fbd0aec9744e4aef8d5e82e3aa5108ead0b
Ammarah-khan <k_ammarah@yahoo.com>
2017-07-24 21:10:39 +0500

    Guest notification (2)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,14 @@ class AppServiceProvider extends ServiceProvider
     {
         // // bug fix : https://laravel-news.com/laravel-5-4-key-too-long-error 
  +        Schema::defaultStringLength(191);
+        view()->composer('frontend.navbar', function($view) {
+            $notify = array();
+            if (\Auth::guest()) {
+                $guest_ip = (strpos(request()->ip(), ':'))? strstr(request()->ip(),':',true) : request()->ip();
+                $notify = \App\File::where([['user_id',null],['ipaddress',$guest_ip],['status', 3]])->get();
+            }
+            $view->with('notify', $notify);
+        });
     }
 
     /**
