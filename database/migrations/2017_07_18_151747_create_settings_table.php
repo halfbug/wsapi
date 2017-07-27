40832290d7fcdd258982d23ab1673742559cccd8
saimaharis <saima.harisahmed@gmail.com>
2017-07-27 16:31:27 +0500

    db migration done

--- database/migrations/2017_07_18_151747_create_settings_table.php
+++ database/migrations/2017_07_18_151747_create_settings_table.php
@@ -13,7 +13,7 @@ class CreateSettingsTable extends Migration
      */
     public function up()
     {
-        Schema::table('settings', function (Blueprint $table) {
+        Schema::create('settings', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('value');
