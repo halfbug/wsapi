10008fc4b882c42af2b1f74d62be46e1839d2f0f
Ammarah-khan <k_ammarah@yahoo.com>
2017-07-28 15:50:26 +0500

    Setting - Add new and Back buttons linked using url

--- resources/views/setting/create.blade.php
+++ resources/views/setting/create.blade.php
@@ -17,7 +17,7 @@
 @endsection
 
 @section('heading')
-    Settings&nbsp;<small><a href="/setting" >Back</a></small>
+    Settings&nbsp;<small><a href="{{url('setting')}}" >Back</a></small>
 @endsection
 @section('content')
     <div class="col-md-12">
